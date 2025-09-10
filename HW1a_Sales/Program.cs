1fbe7f2b3d8bed97884609e053cd614573e604e7
Britten George <Britten.A.George-1@ou.edu>
2025-09-10 12:33:24 -0500

    Finish

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Britten George
+// Did you seek help ? If yes, specify the helper or web link here: I used the slides to remember how to do a horizontal tab
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,27 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("What is the product name of the item you're purchasing?");
+            string itemname = Console.ReadLine();
+
+            Console.WriteLine($"How many {itemname}'s would you like to buy? ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"What is the price for each {itemname}?");
+            double price = double.Parse(Console.ReadLine());
+
+            double subtotal = quantity * price;
+            const double salestax = .085;
+            double salestaxtotal = salestax * quantity;
+            double Total = subtotal + salestaxtotal;
+
+                Console.WriteLine($"\tYour subtotal for you bill is ${subtotal}.");
+                Console.WriteLine($"\tYour sales tax for your bill is ${salestaxtotal}.");
+                Console.WriteLine($"\tYour total for your bill is ${Total}.");
+
+
+            Console.ReadKey();
+            //DONE
+         }
     }
 }
